09c1b953f9500e01f8bad2c06dddf8aca9f1e2c2
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2021-09-27 15:49:07 -0700

    First commit, Version 0

---
+++ src/CoreJavaAssignments/CoreJavaAssignments.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package CoreJavaAssignments;
+
+/**
+ * @author vance
+ *
+ */
+public class CoreJavaAssignments {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
