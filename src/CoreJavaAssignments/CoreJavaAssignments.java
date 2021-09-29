ba9e96b36c444fa9827a0fa1208da388e2cfa269
Kyle Vance <91154892+KVance1010@users.noreply.github.com>
2021-09-29 11:07:26 -0700

    Switch for main menu created and first two assignments entered

--- src/CoreJavaAssignments/CoreJavaAssignments.java
+++ src/CoreJavaAssignments/CoreJavaAssignments.java
@@ -7,14 +7,72 @@ package CoreJavaAssignments;
  * @author vance
  *
  */
+import java.util.Scanner;
+
 public class CoreJavaAssignments {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		int usersChoice;
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("Please pick which assignment you would like to see:  ");
+		System.out.println("                  Options");
+		System.out.println("1: String Statements");
+		System.out.println("2: Student Objects");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.print("Please type you nunber of the option you would like:   ");
+		usersChoice = scnr.nextInt();
+		StringStatement assingmentOne = new StringStatement();
+		
+	switch(usersChoice) {
+	case 1:
+		assingmentOne.stringStatements();
+		break;
+	case 2:
+		Student s1 = new Student ("John", 24, 'S', 'M', 80, 90, 100);
+		Student s2 = new Student ("Mary", 30, 'S', 'F', 60, 40);
+		Student s3 = new Student ("Pete", 40, 'S', 'M', 70, 75);
+		Student s4 = new Student ("Ray ", 35, 'S', 'M', 100, 100, 100);
+		s1.display();
+		s2.display();
+		s3.display();
+		s4.display();
+		
+		break;
+	case 3:
+		
+		break;
+	case 4:
+		
+		break;
+    case 5:
+		
+		break;
+    case 6:
+		
+		break;
+    case 7:
+	
+	    break;
+    case 8:
+	
+	    break;
+    case 9:
+	
+	    break;
+    case 10:
+		
+		break;
+	}
 	}
 
 }
---
+++ src/CoreJavaAssignments/Student.java
@@ -0,0 +1,36 @@
+package CoreJavaAssignments;
+
+public class Student {
+	
+	String name;
+	int age;
+	char section;
+	char gender;
+	int subjectOne = 0;
+	int subjectTwo = 0;
+	int subjectThree = 0;
+	   
+	public Student(String name, int age, char section, char gender, int subjectOne, int subjectTwo, int subjectThree){
+	    this.name = name;
+		this.age = age;
+		this.section = section;
+		this.subjectOne = subjectOne;
+		this.subjectTwo = subjectTwo;
+		this.subjectThree = subjectThree;
+	}
+	public Student(String name, int age, char section, char gender, int subjectTwo, int subjectThree){
+	    this.name = name;
+		this.age = age;
+		this.section = section;
+		this.subjectTwo = subjectTwo;
+		this.subjectThree = subjectThree;
+		   
+	   
+	}
+	public void display() {
+		int totalMarks = (subjectOne + subjectTwo + subjectThree);
+		double testResults = totalMarks / 3.0;
+		System.out.println(name +" " +age + " " + gender + " " + section + " " + testResults);
+	}
+	   	   
+}
